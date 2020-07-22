886edba4cd03dbae60fa268e7df820f5b4bbde7c
zzh <zhenghao0130@icloud.com>
2020-07-22 21:42:57 +0800

    create file

---
+++ swifft/swifft/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  swifft
+//
+//  Created by user on 2020/7/22.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+let str = "asd"
+var str1 = "aaa"
+str1 = "1"
+print(str1)
