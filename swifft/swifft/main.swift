4d1e488c946b339daa59557b7eab48d03b0ee315
zzh <zhenghao0130@icloud.com>
2020-07-29 22:46:08 +0800

    study func

--- swifft/swifft/main.swift
+++ swifft/swifft/main.swift
@@ -70,3 +70,76 @@ case let dec where dec % 10 == 0:
 default:
     print("no")
 }
+
+let admin = "admin"
+let passWordIsValid = true
+switch (admin,passWordIsValid) {
+case ("admin", true):
+    print("welcome admin");
+case ("guest", _):
+    print("guest can't into");
+case (_, true):
+    print("all can into");
+default:
+    print("error");
+}
+
+func simpleFun() {
+    print("here in func")
+}
+
+simpleFun()
+
+func printInt(val:Int) ->Int {
+    print("val is \(val)")
+    var message = "old"
+    if val > 0 {
+        message = message.uppercased();
+        print("message is \(message)")
+        message = message.lowercased();
+    }
+    return val;
+}
+
+printInt(val: 1)
+
+func sendMessage(_ message: String, to: String = "",_ outing: Bool = true) {
+    var msg = message
+    if outing {
+        msg = message.uppercased()
+    }
+    print("\(msg) \(to)")
+    
+}
+
+sendMessage("hello", to: "xb", true)
+sendMessage("hi", false)
+
+func findStringInArray(havingPrefix prefix: String, in strings : [String]) ->String?{
+    for string in strings {
+        if string.hasPrefix(prefix) {
+            return string
+        }
+    }
+    return nil
+}
+
+var str2 = findStringInArray(havingPrefix: "b", in: ["asd", "sss"])
+print("has prefix is \(String(describing: str2))")
+
+func filterInts(_ numbers: [Int], _ includeNumber: (Int)->Bool) -> [Int] {
+    var ret:[Int] = []
+    for num in numbers {
+        if includeNumber(num) {
+            ret.append(num)
+        }
+    }
+    return ret
+}
+
+let numbers = [4, 7, 5]
+//let eventNums = filterInts(numbers, { (num:Int)-> Bool in return num % 2 == 0})
+//let eventNums = filterInts(numbers, {num in return num % 2 == 0})
+//let eventNums = filterInts(numbers, {num in num % 2 == 0})
+//let eventNums = filterInts(numbers, {$0 % 2 == 0})
+let eventNums = filterInts(numbers) {$0 % 2 == 0}
